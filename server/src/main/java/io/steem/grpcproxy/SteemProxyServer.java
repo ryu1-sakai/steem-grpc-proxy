d634ff7125746b632c168d43e1bb93f0b3826ec6
ryu1-sakai <ryu1.sakai@gmail.com>
2018-11-25 03:25:24 +0900

    Introduce SteemJ and implement Login API

--- server/src/main/java/io/steem/grpcproxy/SteemProxyServer.java
+++ server/src/main/java/io/steem/grpcproxy/SteemProxyServer.java
@@ -1,5 +1,6 @@
 package io.steem.grpcproxy;
 
+import com.google.common.collect.ImmutableList;
 import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.grpc.GrpcSerializationFormats;
@@ -7,17 +8,38 @@ import com.linecorp.armeria.server.Server;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.ServiceWithPathMappings;
 import com.linecorp.armeria.server.grpc.GrpcServiceBuilder;
+import eu.bittrade.libs.steemj.communication.CommunicationHandler;
+import eu.bittrade.libs.steemj.configuration.SteemJConfig;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.net.URI;
+import java.util.List;
 
 public class SteemProxyServer {
 
-    public static void main(String[] args) {
+    public static CommunicationHandler createCommunicationHandler() {
+        try {
+            // TODO: Refactor SteemJ's CommunicationHandler to accept configuration
+            URI testNetUrl = URI.create("https://testnet.steemitdev.com");
+            List<Pair<URI, Boolean>> endpoints = ImmutableList.of(Pair.of(testNetUrl, false));
+            SteemJConfig.getInstance().setEndpointURIs(endpoints);
+            return new CommunicationHandler();
+        } catch (Exception e) {
+            throw new RuntimeException(e); // TODO: Error handling
+        }
+    }
+
+    public static Server create() {
         ServiceWithPathMappings<HttpRequest, HttpResponse> service
                 = new GrpcServiceBuilder()
-                .addService(new SteemProxyService())
+                .addService(new SteemProxyService(createCommunicationHandler()))
                 .supportedSerializationFormats(GrpcSerializationFormats.PROTO, GrpcSerializationFormats.JSON)
                 .enableUnframedRequests(true)
                 .build();
-        Server server = new ServerBuilder().http(80).service(service).build();
-        server.start().join();
+        return new ServerBuilder().http(80).service(service).build();
+    }
+
+    public static void main(String[] args) {
+        create().start().join();
     }
 }
--- server/src/main/java/io/steem/grpcproxy/SteemProxyService.java
+++ server/src/main/java/io/steem/grpcproxy/SteemProxyService.java
@@ -1,5 +1,8 @@
 package io.steem.grpcproxy;
 
+import eu.bittrade.libs.steemj.apis.login.LoginApi;
+import eu.bittrade.libs.steemj.base.models.AccountName;
+import eu.bittrade.libs.steemj.communication.CommunicationHandler;
 import io.grpc.stub.StreamObserver;
 import io.steem.grpc.LoginRequest;
 import io.steem.grpc.LoginResponse;
@@ -7,9 +10,22 @@ import io.steem.grpc.SteemServiceGrpc;
 
 public class SteemProxyService extends SteemServiceGrpc.SteemServiceImplBase {
 
+    private final CommunicationHandler communicationHandler;
+
+    public SteemProxyService(CommunicationHandler communicationHandler) {
+        this.communicationHandler = communicationHandler;
+    }
+
     @Override
     public void login(LoginRequest request, StreamObserver<LoginResponse> responseObserver) {
-        responseObserver.onNext(LoginResponse.newBuilder().setSucceeded(false).build());
-        responseObserver.onCompleted();
+        try {
+            AccountName accountName = new AccountName(request.getAccountName());
+            boolean succeeded
+                    = LoginApi.login(communicationHandler, accountName, request.getPassword());
+            responseObserver.onNext(LoginResponse.newBuilder().setSucceeded(succeeded).build());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            responseObserver.onError(e);
+        }
     }
 }
