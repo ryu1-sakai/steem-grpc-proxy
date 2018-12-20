9cf66955c98dba3b97e404f3ac3b1a11266b7e45
ryu1-sakai <ryu1.sakai@gmail.com>
2018-12-20 23:14:32 +0900

    Implement the model AccountCreateOperation

--- client/src/main/java/io/steem/client/model/operation/AccountCreateOperation.java
+++ client/src/main/java/io/steem/client/model/operation/AccountCreateOperation.java
@@ -1,22 +1,61 @@
 package io.steem.client.model.operation;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.ImmutableMap;
+import io.steem.client.SteemClientException;
 import io.steem.client.model.SteemAsset;
+import io.steem.client.model.SteemAuthority;
 import io.steem.client.model.SteemOperation;
+import lombok.Builder;
+import lombok.ToString;
 
 import java.util.Map;
 
+@Builder
+@ToString
 public class AccountCreateOperation extends SteemOperation {
 
-  private final SteemAsset fee;
-  // TODO: Other fields
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-  protected AccountCreateOperation(SteemAsset fee) {
+  private final SteemAsset fee;
+  private final String creator;
+  private final String newAccountName;
+  private final SteemAuthority owner;
+  private final SteemAuthority active;
+  private final SteemAuthority posting;
+  private final String memoKey;
+  private final String jsonMetadata;
+
+  private AccountCreateOperation(SteemAsset fee, String creator, String newAccountName,
+                                 SteemAuthority owner, SteemAuthority active,
+                                 SteemAuthority posting, String memoKey, String jsonMetadata) {
     super("account_create");
     this.fee = fee;
+    this.creator = creator;
+    this.newAccountName = newAccountName;
+    this.owner = owner;
+    this.active = active;
+    this.posting = posting;
+    this.memoKey = memoKey;
+    this.jsonMetadata = jsonMetadata;
   }
 
   @Override
   protected Map<String, Object> getValueMap() {
-    return null; // TODO
+    try {
+      return ImmutableMap.<String, Object>builder()
+          .put("fee", OBJECT_MAPPER.writeValueAsString(fee))
+          .put("creator", creator)
+          .put("new_account_name", newAccountName)
+          .put("owner", owner.compose())
+          .put("active", active.compose())
+          .put("posting", posting.compose())
+          .put("memo_key", memoKey)
+          .put("json_metadata", jsonMetadata)
+          .build();
+    } catch (JsonProcessingException e) {
+      throw new SteemClientException("Failed to compose: " + this, e);
+    }
   }
 }
