47ba11c894fa48fe900f1128e88b036f8a14fceb
ryu1-sakai <ryu1.sakai@gmail.com>
2019-03-02 04:27:03 +0900

    Implement the model LimitOrderCancelOperation

--- client/src/main/java/io/steem/client/model/operation/LimitOrderCancelOperation.java
+++ client/src/main/java/io/steem/client/model/operation/LimitOrderCancelOperation.java
@@ -1,5 +1,38 @@
 package io.steem.client.model.operation;
 
-public class LimitOrderCancelOperation {
-  //FIXME: Implement the class
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.steem.client.model.SteemOperation;
+import io.steem.client.model.util.ObjectMapUtils;
+import lombok.Builder;
+
+import java.util.Map;
+
+public class LimitOrderCancelOperation extends SteemOperation {
+
+  @Builder
+  @lombok.Value
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+  public static class Value {
+    private final String owner;
+    private final long oderid;
+  }
+
+  private final Value value;
+
+  private LimitOrderCancelOperation(Value value) {
+    super("limit_order_cancel");
+    this.value = value;
+  }
+
+  public static LimitOrderCancelOperation of(Value value) {
+    return new LimitOrderCancelOperation(value);
+  }
+
+  @Override
+  protected Map<String, Object> getValueMap() {
+    return ObjectMapUtils.toObjectMap(value);
+  }
 }
---
+++ client/src/test/java/io/steem/client/model/operation/LimitOrderCancelOperationTest.java
@@ -0,0 +1,43 @@
+package io.steem.client.model.operation;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LimitOrderCancelOperationTest {
+
+  @Test
+  public void toCondenserAndAppbase() {
+    // set up
+    String owner = RandomStringUtils.randomAlphanumeric(8);
+    long oderid = RandomUtils.nextLong();
+
+    LimitOrderCancelOperation.Value value = LimitOrderCancelOperation.Value.builder()
+        .owner(owner)
+        .oderid(oderid)
+        .build();
+    LimitOrderCancelOperation sut = LimitOrderCancelOperation.of(value);
+
+    // exercise
+    // exercise
+    List<Object> actualCondenser = sut.toCondenser();
+    Map<String, Object> actualAppbase = sut.toAppbase();
+
+    // verify
+    Map<String, Object> expectedMap = ImmutableMap.<String, Object>builder()
+        .put("owner", owner)
+        .put("oderid", oderid)
+        .build();
+    Map<String, Object> expectedAppbase
+        = ImmutableMap.of("type", "limit_order_cancel", "value", expectedMap);
+
+    assertThat(actualCondenser).containsExactly("limit_order_cancel", expectedMap);
+    assertThat(actualAppbase).isEqualTo(expectedAppbase);
+  }
+}
